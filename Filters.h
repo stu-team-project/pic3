79d550fa174dadb454c214ada51271a8a9e08b6c
Nikita-Babich <nikita.babich.a@gmail.com>
2023-10-20 16:53:38 +0300

    filters_avilable

--- Filters.h
+++ Filters.h
@@ -1,3 +1,19 @@
 #pragma once
 
-#include "T_Project.h"
+//#include "T_Project.h"
+
+#define _filters_connected true
+#define _filters_available 0
+// #define IMAGE Vector<Vector<Pixel>> //sth like that for return type
+
+/* Nikita's part
+IMAGE Filter_CMYK(IMAGE origin, int parameter){
+	//copy origin into a new image Result
+
+	//return Result;
+}
+*/
+
+/*Jan's part
+
+*/
